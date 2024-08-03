ad62c8d8f2278924820f5d1d76c63c5c6a8e6333
vishnuoum <vishnuoumvishnuoum123@gmail.com>
2024-08-03 22:40:26 +0530

    Integrated Oauth security

---
+++ src/main/java/com/app/aarcs/config/ResourceServerConfig.java
@@ -0,0 +1,43 @@
+package com.app.aarcs.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.introspection.NimbusOpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig {
+
+    @Value("${aarcs.client.id}")
+    private String clientId;
+
+    @Value("${aarcs.client.secret}")
+    private String clientSecret;
+
+    @Value("${aarcs.auth.server.url}")
+    private String authServerUrl;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.opaqueToken(opaqueToken ->
+                                opaqueToken.introspector(opaqueTokenIntrospector())
+                        )
+                );
+        return http.build();
+    }
+
+    @Bean
+    public OpaqueTokenIntrospector opaqueTokenIntrospector() {
+        return new NimbusOpaqueTokenIntrospector(authServerUrl, clientId, clientSecret);
+    }
+}
